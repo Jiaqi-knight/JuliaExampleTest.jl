4c7e7673ab729b75dec67a33a4e58671f6091479
Jiaqi-knight <jiaqi_Wang@sjtu.edu.cn>
2020-12-21 12:04:33 +0800

    new test file with test pass
    pass fail in the past file

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 
 @testset "JuliaExampleTest.jl" begin
     # Write your tests here.
-    @test subfunction(2,1) == 7
-    @test subfunction(2,3) == 13
+    @test subfunction(2,1) == 8
+    @test subfunction(2,3) == 12
 
 end
